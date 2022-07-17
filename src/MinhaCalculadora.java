7f78e8183239d23d6cc9b933ef30674c35c424d8
Guilherme Antônio Garibotti <guilherme.garibotti@hotmail.com>
2022-07-17 14:28:03 -0300

    Initial commit

---
+++ src/MinhaCalculadora.java
@@ -0,0 +1,9 @@
+public class MinhaCalculadora {
+
+    public static int soma(int num1, int num2){
+        return num1 + num2;
+
+    }
+
+
+}
---
+++ src/TesteCalculadora.java
@@ -0,0 +1,20 @@
+public class TesteCalculadora {
+
+    static int resulSoma;
+
+    public static void main(String[] args) {
+
+
+      //MinhaCalculadora calc = new MinhaCalculadora();
+      //calc.soma(6,4);
+
+
+
+        resulSoma = MinhaCalculadora.soma(4,5);
+
+
+        System.out.println(resulSoma);
+
+
+    }
+}
